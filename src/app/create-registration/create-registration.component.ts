e8fd2ed39c9c741a9c66d26e8801092f29af629a
FFBar <gizey.fp@gmail.com>
2023-10-06 23:41:07 +0200

    feat: add calculateBMI logic

--- src/app/create-registration/create-registration.component.ts
+++ src/app/create-registration/create-registration.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-create-registration',
@@ -9,14 +9,20 @@ import {FormBuilder, FormGroup} from "@angular/forms";
 export class CreateRegistrationComponent implements OnInit {
   packages: string[] = ['Monthly', 'Quarterly', 'Half Yearly', 'Yearly'];
   genders: string[] = ['Male', 'Female'];
-  joinReasons: string[] = ['Toxic Fat reduction', 'Weight Loss', 'Weight Gain', 'Muscle Gain', 'General Fitness', 'Others'];
+  joinReasons: string[] = [
+    'Toxic Fat reduction',
+    'Weight Loss',
+    'Weight Gain',
+    'Muscle Gain',
+    'General Fitness',
+    'Others',
+  ];
 
   // Tracks the value and validity state of a group of FormControl instances
   registerForm!: FormGroup;
 
   // FormBuilder is a service that provides syntactic sugar that shortens creating instances of a FormControl, FormGroup, or FormArray
-  constructor(private fb: FormBuilder) {
-  }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.registerForm = this.fb.group({
@@ -39,5 +45,36 @@ export class CreateRegistrationComponent implements OnInit {
 
   submit() {
     console.log(this.registerForm.value);
+    this.calculateBMI();
+  }
+
+  calculateBMI() {
+    const weight = this.registerForm.value.weight;
+    const height = this.registerForm.value.height;
+
+    if (height > 0 && weight > 0) {
+      const finalBmi = weight / (height * height);
+      this.registerForm.controls['bmi'].patchValue(finalBmi.toFixed(2));
+
+      switch (true) {
+        case finalBmi < 18.5:
+          this.registerForm.controls['bmiResult'].patchValue('Underweight');
+          break;
+        case finalBmi > 18.5 && finalBmi < 25:
+          this.registerForm.controls['bmiResult'].patchValue('Normal');
+          break;
+        case finalBmi > 25 && finalBmi < 30:
+          this.registerForm.controls['bmiResult'].patchValue('Overweight');
+          break;
+        case finalBmi > 30:
+          this.registerForm.controls['bmiResult'].patchValue('Obese');
+          break;
+        default:
+          this.registerForm.controls['bmiResult'].patchValue('Invalid');
+      }
+    } else {
+      this.registerForm.controls['bmiResult'].patchValue('Invalid');
+      this.registerForm.controls['bmi'].patchValue('Invalid');
+    }
   }
 }
